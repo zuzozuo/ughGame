1a54512e1dbff78f6392c3fc5981380d7e10af18
nolensvolens <duraledsedlex.xx@gmail.com>
2018-06-05 23:31:45 +0200

    Clear canvas

---
+++ js/game.js
@@ -0,0 +1,21 @@
+class Game{
+    constructor(context, height, width){
+        this.ctx = context;
+        this.width = width;
+        this.height = height;
+    }
+
+    setup(){
+
+    }
+
+    update(){
+        
+    }
+
+    render(){
+        
+    }
+
+    
+}
---
+++ js/main.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+	const FOREVER = window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		function(callback) { return setTimeout(callback, 1000 / 60); };
+
+	let frontCanvas = document.getElementById('gameWindow');
+	let frontContext = frontCanvas.getContext('2d');
+
+	let backCanvas = document.createElement('canvas');
+	let backContext = backCanvas.getContext('2d');
+
+	let width = frontCanvas.width;
+	let height = frontCanvas.height;
+
+	backCanvas.width = width;
+	backCanvas.height = height;
+
+	let game = new Game(backContext, width, height);
+	game.setup();
+
+	function mainloop() {
+        backContext.fillStyle = 'blue';
+        backContext.fillRect(0, 0, width, height);
+		game.update();
+		game.render();
+		frontContext.drawImage(backCanvas, 0, 0);
+		FOREVER(mainloop);
+	}
+
+	mainloop();
+
+});
